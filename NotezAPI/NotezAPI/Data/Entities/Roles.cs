d0e329607867195f0763f296e84f8295003857e4
JakeHeld <jakeheld123@gmail.com>
2020-11-14 13:17:20 -0600

    identity is configed

---
+++ NotezAPI/NotezAPI/Data/Entities/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace NotezAPI.Data.Entities
+{
+    internal static class Roles
+    {
+        public const string Lecturer = nameof(Lecturer);
+        public const string Student = nameof(Student);
+        public const string StudentPLus = Lecturer + "," + Student;
+    }
+}
---
+++ NotezAPI/NotezAPI/Data/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace NotezAPI.Data.Entities
+{
+    public class User: IdentityUser<int>
+    {
+        public virtual ICollection<UserRole> Roles { get; set; } = new List<UserRole>();
+    }
+}
